b9bb2e25f5bd1b83540dc748af60a3880c9f270f
AbelMarco <cranchcps@gmail.com>
2018-10-02 18:48:13 +0200

    Se incluyen Templates

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const { consoleLogMiddleware, fileLogMiddleware } = require('./middleware.js')
+
+const hbs = require('hbs')
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+app.set('view engine', 'hbs') // clave valor
 
 const port = 3000
 
 app.use(consoleLogMiddleware)
 app.use(fileLogMiddleware)
 
-app.get('/', (req, res) => { res.send('Esto funciona') })
-app.get('/contactar', (req, res) => { res.send({ nombre: 'Pepe', apellido: 'Martinez' }) })
+const staticRoute = path.join(__dirname, 'public')
+app.use(express.static(staticRoute))
+
+app.get('/home', (req, res) => {
+  res.render('home', {
+    pageTitle: 'Mi Web',
+    siteWeb: 'Bienvenidos a la web de',
+    pageBody: 'Esta es mi página personal',
+    currentYear: new Date().getFullYear(),
+    author: 'Abel Marco'
+  })
+})
+app.get('/contactar', (req, res) => {
+  res.render('contactar', {
+    pageTitle: 'Contactar',
+    currentYear: new Date().getFullYear(),
+    author: 'Abel Marco'
+  })
+})
 app.listen(port, () => console.log(`Se ha levantado servidor en el puerto ${port}`))
